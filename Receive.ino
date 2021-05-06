77f3de028b6dfb41ac3973ad0513b6ad58b5b513
Breanne Pundsack <25271535+BreanneP@users.noreply.github.com>
2021-05-06 18:25:06 -0500

    Update Receive.ino

--- Receive.ino
+++ Receive.ino
@@ -1,41 +1,39 @@
-#include "mcp_can.h"
-#include <SPI.h>
-#include <LiquidCrystal.h>
+#include <can.h>
+#include <mcp2515.h>
 
-LiquidCrystal lcd(9, 8, 7, 6, 5, 4);
+//set up the data structure for reading the data
+struct can_frame canMsg;
+
+//create an instance of MCP2515 class
+MCP2515 mcp2515(10);
 
 void setup() {
-  lcd.begin(20, 4);
-  CAN.begin(CAN_100KBPS);                   // init can bus : baudrate = 100k
-  attachInterrupt(0, MCP2515_ISR, FALLING); // start interrupt
-  Serial.begin(115200);
+  Serial.begin(9600);
+
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ); //sets CAN at speed 500KBPS
+  mcp2515.setNormalMode();
+
+  canMsg.can_id = 0x036; //CAN id as 0x036
+  canMsg.can_dlc = 2; //CAN data length as 2 bytes
 }
 
-
 void loop() {
-    CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
-    lcd.setCursor(0, 0);
-    
-    
-     //This loops through each byte of data and prints it
-     for(int i = 0; i<len; i++) {
+  mcp2515.readMessage(&canMsg);
 
-        if(i == 0) { //this is the temperature sensor
-          float voltage = ((buf[0] * 4) / 1024 ) * 5.0;
-          float tempC = (voltage - 0.5) * 100;
-          float tempF = ((tempC * 9) / 5.0) + 32.0;
-          lcd.print(tempF);
-        }
-        
-        if(i == 1) { //this is the level sensor
-          if(buf[1] == 1) 
-            Serial.print("Level is okay");
-          else
-            Serial.print("Level is not okay");
-        }
-     }
-     
-      Serial.println();
-      delay(50);
-    }
+  //read the temperature value
+  float voltage = (canMsg.data[0] / 256.0) * 5.0;
+  float tempC = (voltage - 0.5) * 100;
+  float tempF = ((tempC * 9) / 5.0) + 32.0;
+  Serial.print("temperature is ");
+  Serial.print(tempF);
+  Serial.println("\n");
+
+  //read the level value
+  if(canMsg.data[1] == 1)
+    Serial.print("The level is okay!");
+  else
+    Serial.print("The level is not okay!");
+  
+  delay(200);
 }
