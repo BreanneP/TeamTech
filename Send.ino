083947d42187cd4feb24ef8126c64cbed7ba87a6
Breanne Pundsack <25271535+BreanneP@users.noreply.github.com>
2021-05-21 11:33:16 -0500

    Update Send.ino

--- Send.ino
+++ Send.ino
@@ -2,7 +2,9 @@
 #include <mcp2515.h>
 
 //set up the data structure for sending the data
-struct can_frame canMsg;
+struct can_frame canMsg1;
+struct can_frame canMsg2;
+struct can_frame canMsg3;
 
 //create an instance of MCP2515 class
 MCP2515 mcp2515(10);
@@ -14,14 +16,22 @@ void setup() {
   mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ); //sets CAN at speed 500KBPS
   mcp2515.setNormalMode();
 
-  canMsg.can_id = 0x036; //CAN id as 0x036
-  canMsg.can_dlc = 1; //CAN data length as 1 byte
+  canMsg1.can_id  = 0xAA;
+  canMsg1.can_dlc = 8;
+
+  canMsg2.can_id  = 0xBB;
+  canMsg2.can_dlc = 8;               
+
+  canMsg3.can_id  = 0xCC;
+  canMsg3.can_dlc = 8;
 
 }
 
 void loop() {
   //send the message
-  mcp2515.sendMessage(&canMsg);
+  mcp2515.sendMessage(&canMsg1);
+  mcp2515.sendMessage(&canMsg2);
+  mcp2515.sendMessage(&canMsg3);
     
   delay(200);
 
