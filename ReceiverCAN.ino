f0f994447036a0199b9f8467e0e4740d9d426b8c
Breanne Pundsack <25271535+BreanneP@users.noreply.github.com>
2021-06-03 11:46:28 -0500

    Update ReceiverCAN.ino

--- ReceiverCAN.ino
+++ ReceiverCAN.ino
@@ -1,45 +1,112 @@
-#include <SPI.h>          //SPI is used to talk to the CAN Controller
-#include <mcp2515_can.h>
+/*
+ * demo: CAN-BUS Shield, receive all frames and print all fields id/type/data
+ * to receive frame fastly, a poll in loop() is required.
+ *
+ * Copyright (C) 2020 Seeed Technology Co.,Ltd.
+ */
+#include <SPI.h>
 
-mcp2515_can CAN(10);          //set SPI Chip Select to pin 10
+#define CAN_2515
+// #define CAN_2518FD
 
-unsigned char len = 0;
-unsigned char buf[8];
-unsigned int canID;
+// Set SPI CS Pin according to your hardware
+
+#if defined(SEEED_WIO_TERMINAL) && defined(CAN_2518FD)
+// For Wio Terminal w/ MCP2518FD RPi Hat：
+// Channel 0 SPI_CS Pin: BCM 8
+// Channel 1 SPI_CS Pin: BCM 7
+// Interupt Pin: BCM25
+const int SPI_CS_PIN  = BCM8;
+const int CAN_INT_PIN = BCM25;
+#else
+
+// For Arduino MCP2515 Hat:
+// the cs pin of the version after v1.1 is default to D9
+// v0.9b and v1.0 is default D10
+const int SPI_CS_PIN = 9;
+const int CAN_INT_PIN = 2;
+#endif
+
+
+#ifdef CAN_2518FD
+#include "mcp2518fd_can.h"
+mcp2518fd CAN(SPI_CS_PIN); // Set CS pin
+
+// To TEST MCP2518FD CAN2.0 data transfer
+#define MAX_DATA_SIZE 8
+// To TEST MCP2518FD CANFD data transfer, uncomment below lines
+// #undef  MAX_DATA_SIZE
+// #define MAX_DATA_SIZE 64
+
+#endif
+
+#ifdef CAN_2515
+#include "mcp2515_can.h"
+mcp2515_can CAN(SPI_CS_PIN); // Set CS pin
+#define MAX_DATA_SIZE 8
+#endif
 
 void setup() {
-  Serial.begin(9600);   //to communicate with Serial monitor
-  SPI.begin();
-  
-//tries to initialize, if failed --> it will loop here for ever
-START_INIT:
+    SERIAL_PORT_MONITOR.begin(115200);
+    while (!SERIAL_PORT_MONITOR) {}
 
-    if(CAN_OK == CAN.begin(CAN_500KBPS))    //setting CAN baud rate to 500Kbps
-        Serial.println("CAN BUS Shield init ok!");
-        
-    else {
-        Serial.println("CAN BUS Shield init fail");
-        Serial.println("Init CAN BUS Shield again");
+    #if MAX_DATA_SIZE > 8
+    /*
+     * To compatible with MCP2515 API,
+     * default mode is CAN_CLASSIC_MODE
+     * Now set to CANFD mode.
+     */
+    CAN.setMode(CAN_NORMAL_MODE);
+    #endif
+
+    while (CAN_OK != CAN.begin(CAN_500KBPS)) {             // init can bus : baudrate = 500k
+        SERIAL_PORT_MONITOR.println(F("CAN init fail, retry..."));
         delay(100);
-        goto START_INIT;
     }
+    SERIAL_PORT_MONITOR.println(F("CAN init ok!"));
 }
 
-
+uint32_t id;
+uint8_t  type; // bit0: ext, bit1: rtr
+uint8_t  len;
+byte cdata[MAX_DATA_SIZE] = {0};
 
 void loop() {
-    if(CAN_MSGAVAIL == CAN.checkReceive()) {    //check if data is coming
-        CAN.readMsgBuf(&len, buf);    //read data,  len: data length, buf: data buffer
-        canID = CAN.getCanId();       //getting the ID of the incoming message
-
-        Serial.print("ID is: ");
-        Serial.print(canID, HEX);     //printing the ID in its standard form, HEX
-
-        Serial.print("    Length is: ");
-        Serial.println(len);
-        
-        for(int i = 0; i<len; i++)    //looping on the incoming data to print them
-            Serial.write(buf[i]);     //Serial.write prints the character itself
-        Serial.println("\n\t*****************\n");
+    // check if data coming
+    if (CAN_MSGAVAIL != CAN.checkReceive()) {
+        return;
     }
+
+    char prbuf[32 + MAX_DATA_SIZE * 3];
+    int i, n;
+
+    unsigned long t = millis();
+    // read data, len: data length, buf: data buf
+    CAN.readMsgBuf(&len, cdata);
+
+    id = CAN.getCanId();
+    type = (CAN.isExtendedFrame() << 0) |
+           (CAN.isRemoteRequest() << 1);
+    /*
+     * MCP2515(or this driver) could not handle properly
+     * the data carried by remote frame
+     */
+
+    n = sprintf(prbuf, "%04lu.%03d ", t / 1000, int(t % 1000));
+    /* Displayed type:
+     *
+     * 0x00: standard data frame
+     * 0x02: extended data frame
+     * 0x30: standard remote frame
+     * 0x32: extended remote frame
+     */
+    static const byte type2[] = {0x00, 0x02, 0x30, 0x32};
+    n += sprintf(prbuf + n, "RX: [%08lX](%02X) ", (unsigned long)id, type2[type]);
+    // n += sprintf(prbuf, "RX: [%08lX](%02X) ", id, type);
+
+    for (i = 0; i < len; i++) {
+        n += sprintf(prbuf + n, "%02X ", cdata[i]);
+    }
+    SERIAL_PORT_MONITOR.println(prbuf);
 }
+// END FILE
